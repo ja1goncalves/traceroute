b51d803803f443d7ff92f08a134ccb484feb7692
Joao Paulo Felix <jpfgs@ecomp.poli.br>
2019-10-18 04:09:48 +0000

    emojis

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 Press [ENTER] to continue or Ctrl-c to cancel adding it.
 
 sudo apt install python3.7
+pip install emoji --upgrade
 ```
 Alternatives `pygeoip`:
 ```
--- Traceroute.py
+++ Traceroute.py
@@ -6,17 +6,25 @@ import time
 import socket
 import struct
 import sys
+import signal
+
+
+def signal_handler(sig, frame):
+    print('Até mais! \U0001f600 \U0001F618 \U0001F44B')
+    sys.exit(0)
 
 
 def main():
-    print('-' * 40)
-    print("Iniciando o footprint do alvo")
-    print('-' * 40)
+    print('-' * 60)
+    print("Iniciando o footprint do alvo | Pressione Ctrl+C para sair")
+    print('-' * 60)
 
     endpoint = input("Digite o site do alvo: ")
     jumps = int(input("Digite a quantidade saltos: "))
 
+    signal.signal(signal.SIGINT, signal_handler)
     tracer(endpoint, jumps)
+    signal.pause()
 
 
 def tracer(host, jumps=30):
@@ -27,9 +35,9 @@ def tracer(host, jumps=30):
     try:
         destine = socket.gethostbyname(host)  # get host request destine
     except socket.error as e:
-        raise IOError(f'Unable to resolve {host}:{e}')
+        raise IOError(f'\U0001F912 \U0001F915 Unable to resolve {host}:{e}')
 
-    print(f"Traceroute to {destine} ({host}:{port}) with {jumps} jumps")
+    print(f"Traceroute to {destine} ({host}:{port}) with {jumps} jumps \U0001F9D0")
 
     while True:
         receiver = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)  # server received information for package
@@ -38,53 +46,57 @@ def tracer(host, jumps=30):
         sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # server send package
         sender.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)  # define socket's params for IP and TTL
 
-        receiver.bind(('', port))  # init received server
-        sender.sendto(b"", (host, port)) # send package in bytes by send server
+        try:
+            receiver.bind(('', port))  # init received server
+            sender.sendto(b"", (host, port))  # send package in bytes by send server
 
-        start_time = time.time()  # get start time for calculation delay
-        finished = False  # verify receive package for finish
-        tries = 3  # packet send and receive attempt numbers
-        addr_present = None  # ip in address stoped packege
-        host_present = None  # host of ip
+            start_time = time.time()  # get start time for calculation delay
+            finished = False  # verify receive package for finish
+            tries = 3  # packet send and receive attempt numbers
+            addr_present = None  # ip in address stoped packege
+            host_present = None  # host of ip
 
-        sys.stdout.write(f'ttl:{ttl:<8} ')
+            sys.stdout.write(f'ttl:{ttl:<8} ')
 
-        while not finished and tries > 0:  # while the attempts are not over
-            try:
-                _, addr_present = receiver.recvfrom(512)  # Receive package in addr_present [index 1]
-                finished = True  # indicates the receipt the package
-                end_time = time.time()  # get end time for calculation delay
-                addr_present = addr_present[0]  # first of ip list [last package passed]
-
+            while not finished and tries > 0:  # while the attempts are not over
                 try:
-                    host_present = socket.gethostbyaddr(addr_present)[0]  # get host by ip address
-                except socket.error:
-                    host_present = addr_present
+                    _, addr_present = receiver.recvfrom(512)  # Receive package in addr_present [index 1]
+                    finished = True  # indicates the receipt the package
+                    end_time = time.time()  # get end time for calculation delay
+                    addr_present = addr_present[0]  # first of ip list [last package passed]
 
-            except socket.error as e:
-                tries = tries - 1
-                sys.stdout.write("* ")
+                    try:
+                        host_present = socket.gethostbyaddr(addr_present)[0]  # get host by ip address
+                    except socket.error:
+                        host_present = addr_present
 
-        receiver.close()  # close receive server
-        sender.close()  # close send server
+                except socket.error as e:
+                    tries = tries - 1
+                    sys.stdout.write("\U0001F62A ")
 
-        if not finished:
-            pass
+            receiver.close()  # close receive server
+            sender.close()  # close send server
 
-        if addr_present is not None:
-            time_delay = round((end_time - start_time) * 1000, 2)  # calculation the time delay
-            sys.stdout.write(f'host: {host_present:<45} ')
-            sys.stdout.write(f'IP: {addr_present:<17} ')
-            sys.stdout.write(f'timedelay: {time_delay}ms ')
-            print()
-            # print(f'host: {host_present}\t\t\tIP:({addr_present})\ttimedelay: {time_delay}ms')
-        else:
-            print('*' * 4)
+            if not finished:
+                pass
 
-        ttl += 1
-        if addr_present == destine or ttl > jumps:  # packege has arrived in destine or jumps is finished
-            print("Análise feita com SUCESSO")
-            break
+            if addr_present is not None:
+                time_delay = round((end_time - start_time) * 1000, 2)  # calculation the time delay
+                sys.stdout.write(f'host: {host_present:<45} ')
+                sys.stdout.write(f'IP: {addr_present:<17} ')
+                sys.stdout.write(f'timedelay: {time_delay}ms ')
+                print()
+                # print(f'host: {host_present}\t\t\tIP:({addr_present})\ttimedelay: {time_delay}ms')
+            else:
+                print('\U0001F62A' * 4)
+
+            ttl += 1
+            if addr_present == destine or ttl > jumps:  # packege has arrived in destine or jumps is finished
+                print("Análise feita com SUCESSO")
+                break
+        except (socket.gaierror or socket.error) as e:
+            print("Algo de errado ocorreu com envio ou recebimento de informações \U0001F912 \U0001F915")
+            print(f"Mensagem de erro: {str(e)}")
 
 
 if __name__ == '__main__':
